06e7a3c5053529c799a6f6ba4a9300e57b4d862a
NicoCurtis <nicholaskyereboah@gmail.com>
2024-07-31 04:12:46 +0000

    Log Out Button Added

--- STUDENT-HELPDESK/src/app/LoginPage/login/login.component.ts
+++ STUDENT-HELPDESK/src/app/LoginPage/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent {
     this.authService.login(this.username, this.password).subscribe(
       (data) => {
         this.authService.setSession(data);
-        this.router.navigate(['/dashboard']);
+        this.router.navigate(['/dashmain']);
       },
       (error) => {
         console.error('Error logging in', error);
