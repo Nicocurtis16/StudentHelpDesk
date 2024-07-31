6e9b99a1aa482c9aca7c77cb6c3b1a33d4498926
NicoCurtis <nicholaskyereboah@gmail.com>
2024-07-31 13:11:13 +0000

    moved project files to root folder

---
+++ src/app/Dashboard/dashlayout/dashlayout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashlayout',
+  templateUrl: './dashlayout.component.html',
+  styleUrl: './dashlayout.component.css'
+})
+export class DashlayoutComponent {
+
+}
